912a094f6ecb1905c7d4c9f39b3611228d966b30
opsupi <98511043+opsupi@users.noreply.github.com>
2023-11-23 16:44:59 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,22 @@
 // This is where your JavaScript code will go
+// Fetch your AI-generated image URLs here and replace the placeholders
+const imageUrls = [
+    "https://via.placeholder.com/300",
+    "https://via.placeholder.com/300",
+    "https://via.placeholder.com/300"
+];
+
+// Function to dynamically create image elements
+function createImageElements() {
+    const imageContainer = document.querySelector('.image-container');
+
+    imageUrls.forEach(url => {
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = 'AI Image';
+        imageContainer.appendChild(img);
+    });
+}
+
+// Call the function to create image elements
+createImageElements();
